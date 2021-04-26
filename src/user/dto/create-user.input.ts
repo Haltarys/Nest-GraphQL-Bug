989210c32c46e1467ae9b2d430e048284d78feb0
Haltarys <gregoire.haardt@gmail.com>
2021-04-27 00:33:15 +0200

    Make validators optional

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,10 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsArray, IsDefined } from 'class-validator';
+import { IsArray, IsDefined, ValidateIf } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @IsDefined()
   @IsArray()
+  @ValidateIf(() => {
+    // change this to true to enable validators
+    return false;
+  })
   @Field((type) => [String], { nullable: false, defaultValue: [] })
   details: string[];
 }
